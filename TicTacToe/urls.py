cfcf46a687bb6c9e2ecbc452a42f99bb313bac95
saidislam2008 <saidislammamasaliev@gmail.com>
2024-05-31 01:44:34 +0600

    altered minor changes

--- TicTacToe/urls.py
+++ TicTacToe/urls.py
@@ -19,11 +19,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from game.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('product.urls')),
-    path('user/', include('user.urls')),
+    path('', index, name='index'),
 ]
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
